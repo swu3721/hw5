91a954e98e2a66b2b533b3b561db7577ef9f036e
Samuel Wu <samuelsw@usc.edu>
2024-04-14 20:10:12 -0700

    completed schedwork, wordle

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, size_t day, size_t workersNeeded, size_t numWorkers, std::vector<int>& workerShiftsDone, std::vector<std::vector<bool>>& workerWorkedOnDay);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +38,60 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    //Number of workers
+    size_t numWorkers = avail[0].size();
+    //Vector with number of shifts done by each worker (index)
+    std::vector<int> workerShiftsDone(numWorkers, 0);
+
+    //2D vector to verify if worker has already worked on the day (avail.size())
+    std::vector<std::vector<bool>> workerWorkedOnDay(avail.size(), std::vector<bool>(numWorkers, false));
+
+    for (size_t i = 0; i < avail.size(); i++) { //For each day
+        std::vector<unsigned int> temp; //Make a temp variable
+        for (size_t j = 0; j < dailyNeed; j++) { //For dailyNeeded workers
+            temp.push_back(0); //Push back 0
+        }
+        sched.push_back(temp); //Push back temp
+    }
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, numWorkers, workerShiftsDone, workerWorkedOnDay);
+
 
 
 
 }
 
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, size_t day, size_t workersNeeded, size_t numWorkers, std::vector<int>& workerShiftsDone, std::vector<std::vector<bool>>& workerWorkedOnDay) {
+    if (day == avail.size()) { //If all days observed
+        return true;
+    }
+
+    for (size_t k = 0; k < numWorkers; ++k) { //For each worker
+        if (avail[day][k] == true && workerWorkedOnDay[day][k] == false && workerShiftsDone[k] < maxShifts) { //If the worker can work that day and it is valid (not exceeding max shifts)
+            
+            sched[day][workersNeeded] = k; //Add k to the schedule (schedule k for shift on day)
+            workerShiftsDone[k] += 1; //Add to k's shifts done
+            workerWorkedOnDay[day][k] = true; //Set worker k on day to true
+
+            // Recursive call for the next day
+            int newDay = day;
+            int newWorkersNeeded = workersNeeded;
+            if (workersNeeded + 1 == dailyNeed) { //If we are at the max workers for the day, move to the next day
+                newDay++;
+                newWorkersNeeded = 0;
+            } else { //Else move to next worker (d)
+                newWorkersNeeded++;
+            }
+
+            if (scheduleHelper(avail, dailyNeed, maxShifts, sched, newDay, newWorkersNeeded, numWorkers, workerShiftsDone, workerWorkedOnDay)) {
+                return true;
+            }
+
+            //Backtrack
+            workerWorkedOnDay[day][k] = false; //Set worker k to false
+            workerShiftsDone[k] -= 1; //Subtract from k's shift
+        }
+        
+    }
+    //if we get here, no workers possible for that day
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void generateWords(const std::string& fixed, const std::string& floating, const std::string& alphabet, std::string word, const std::set<std::string>& dict, int index, int numFloating, int numBlank, std::set<std::string>& result);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,52 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    std::set<std::string> result;
 
+    int numBlank = 0;
+    for (char c : in) {
+        if (c == '-') {
+            numBlank++;
+        }
+    }
+    int numFloating = floating.length();
+    generateWords(in, floating, alphabet, "", dict, 0, numFloating, numBlank, result);
+    return result;
 }
 
 // Define any helper functions here
+// Generate all combos of words given in (in), floating (floating), and dict, with word being the word generated and result being all words generated
+void generateWords(const std::string& in, const std::string& floating, const std::string& alphabet, std::string word, const std::set<std::string>& dict, int index, int numFloating, int numBlank, std::set<std::string>& result) {
+    
+    
+    if (in[index] != '-' && index < in.length()) { //If the current position is fixed
+        word = word + in[index];
+        generateWords(in, floating, alphabet, word, dict, index + 1, numFloating, numBlank, result);
+    }
+
+    if (index == in.length()) { //If the word made is the same length as fixed,
+        if (numFloating == 0 && dict.find(word) != dict.end()) { //If the word is found
+            result.insert(word);
+        }
+        return;
+    }
+    
+
+    for(size_t i = 0; i < floating.size(); i++) {
+        word = word + floating[i];
+        std::string newFloating = floating;
+        newFloating.erase(i, 1);
+        generateWords(in, newFloating, alphabet, word, dict, index + 1, numFloating - 1, numBlank - 1, result);
+        newFloating.insert(i, 1, floating[i]);
+        word.pop_back();
+    }
+
+    if (numBlank > numFloating) {
+        for (char c : alphabet) {
+            word = word + c;
+            generateWords(in, floating, alphabet, word, dict, index + 1, numFloating, numBlank - 1, result);
+            word.pop_back();
+        }
+    }
+}
